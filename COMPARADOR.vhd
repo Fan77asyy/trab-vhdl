e5a3c7e5028796d6d98b4624b62a2a782d957b53
gpradotxt <guilhermeprado245@gmail.com>
2025-11-27 11:47:24 -0300

    Update COMPARADOR.vhd

--- COMPARADOR.vhd
+++ COMPARADOR.vhd
@@ -6,8 +6,8 @@ use ieee.numeric_std.all;
 
 entity comparador is
     port (
-        A_in   : in  std_logic_vector(8 downto 0);  -- NÃºmero sorteado (A2 A1 A0)
-        B_in   : in  std_logic_vector(8 downto 0);  -- Tentativa do usuÃ¡rio (B2 B1 B0)
+        A_in   : in  std_logic_vector(8 downto 0);  -- Numero sorteado (A2 A1 A0)
+        B_in   : in  std_logic_vector(8 downto 0);  -- Tentativa do usuario (B2 B1 B0)
         R_out  : out std_logic_vector(5 downto 0)   -- Resultados: r2(1:0), r1(1:0), r0(1:0)
     );
 end entity;
@@ -18,7 +18,7 @@ architecture rtl of comparador is
     
     signal r0, r1, r2 : std_logic_vector(1 downto 0);
 begin
-    -- Separa os dÃ­gitos
+    -- Separa os di­gitos
     A0 <= A_in(2 downto 0);
     A1 <= A_in(5 downto 3);
     A2 <= A_in(8 downto 6);
@@ -27,34 +27,34 @@ begin
     B1 <= B_in(5 downto 3);
     B2 <= B_in(8 downto 6);
     
-    -- LÃ³gica de comparaÃ§Ã£o para cada dÃ­gito
+    -- Lógica de comparacao para cada digito
     process(A0, A1, A2, B0, B1, B2)
     begin
-        -- ComparaÃ§Ã£o DÃ­gito 0
+        -- Comparacao Di­gito 0
         if B0 = A0 then
-            r0 <= "10";  -- NÃºmero correto na posiÃ§Ã£o correta
+            r0 <= "10";  -- Numero correto na posicao correta
         elsif B0 = A1 or B0 = A2 then
-            r0 <= "01";  -- NÃºmero correto na posiÃ§Ã£o errada
+            r0 <= "01";  -- Numero correto na posicao errada
         else
-            r0 <= "00";  -- NÃºmero nÃ£o existe
+            r0 <= "00";  -- Numero nao existe
         end if;
         
-        -- ComparaÃ§Ã£o DÃ­gito 1
+        -- comparacao digito 1
         if B1 = A1 then
-            r1 <= "10";  -- NÃºmero correto na posiÃ§Ã£o correta
+            r1 <= "10";  -- Numero correto na posicao correta
         elsif B1 = A0 or B1 = A2 then
-            r1 <= "01";  -- NÃºmero correto na posiÃ§Ã£o errada
+            r1 <= "01";  -- Numero correto na posicao errada
         else
-            r1 <= "00";  -- NÃºmero nÃ£o existe
+            r1 <= "00";  -- Numero nao existe
         end if;
         
-        -- ComparaÃ§Ã£o DÃ­gito 2
+        -- comparacao digito 2
         if B2 = A2 then
-            r2 <= "10";  -- NÃºmero correto na posiÃ§Ã£o correta
+            r2 <= "10";  -- Numero correto na posicao correta
         elsif B2 = A0 or B2 = A1 then
-            r2 <= "01";  -- NÃºmero correto na posiÃ§Ã£o errada
+            r2 <= "01";  -- Numero correto na posicao errada
         else
-            r2 <= "00";  -- NÃºmero nÃ£o existe
+            r2 <= "00";  -- Numero nao existe
         end if;
     end process;
     
