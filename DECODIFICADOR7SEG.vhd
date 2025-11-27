26c1adbbe6569af5764081fc2b85d124ab38b0ac
Júlia Souza Magalhães <juliamagalhaes2103@gmail.com>
2025-11-27 09:55:35 -0300

    att Decodificador

--- DECODIFICADOR7SEG.vhd
+++ DECODIFICADOR7SEG.vhd
@@ -1,25 +1,25 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
--- DECODIFICADOR 7 SEGMENTOS (SAÍDAS 0 / 1 / 2)
+-- DECODIFICADOR 7 SEGMENTOS (SAÃDAS 0 / 1 / 2)
 
 entity decod7seg is
-    port(
-        code : in  std_logic_vector(1 downto 0);
-        hex  : out std_logic_vector(6 downto 0)
-    );
+    port(
+        code : in  std_logic_vector(1 downto 0);
+        hex  : out std_logic_vector(6 downto 0)
+    );
 end entity;
 
 architecture rtl_dec of decod7seg is
 begin
     -- 7-segmentos Ativo-Baixo:
     -- 0 = 1000000 (Nenhum acerto)
-    -- 1 = 1111001 (Existe em outra posição)
-    -- 2 = 0100100 (Acerto em posição)
+    -- 1 = 1111001 (Existe em outra posiÃ§Ã£o)
+    -- 2 = 0100100 (Acerto em posiÃ§Ã£o)
     
-    with code select
-        hex <= "1000000" when "00", -- 0
-               "1111001" when "01", -- 1
-               "0100100" when "10", -- 2
-               "1111111" when others; -- Apagado
+    with code select
+        hex <= "1000000" when "00", -- 0
+               "1111001" when "01", -- 1
+               "0100100" when "10", -- 2
+               "1111111" when others; -- Apagado
 end architecture;
